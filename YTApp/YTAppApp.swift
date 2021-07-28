395b86a6cb44ed1b2c415a434f1adbc34b2ed2e3
Aaron Ward <Lord>
2021-07-28 03:17:11 -0400

    Initial Commit

---
+++ YTApp/YTAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YTAppApp.swift
+//  YTApp
+//
+//  Created by Aaron Ward on 2021-07-28.
+//
+
+import SwiftUI
+
+@main
+struct YTAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
